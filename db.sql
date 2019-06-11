4818335b5247c002762f6a6813c97de14962e2d4
a_tretyakov <artem.tretyakov.91@inbox.ru>
2019-06-11 10:10:28 +0300

    fix sql

--- db.sql
+++ db.sql
@@ -1,5 +1,48 @@
+CREATE DATABASE test_db;
+
+use test_db;
+
 CREATE TABLE `seq` (
   `name` varchar(20) NOT NULL,
   `val` int(10) unsigned NOT NULL,
   PRIMARY KEY  (`name`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1
+) ENGINE=InnoDB
+  DEFAULT CHARSET=utf8
+  COLLATE=utf8_general_ci;
+
+
+CREATE TABLE test_db.oms_orders_1 (
+	id varchar(100) NOT NULL,
+	invoice_id varchar(100) NOT NULL,
+	order_address varchar(100) NULL,
+	created_at DATETIME NOT NULL
+)
+  ENGINE=InnoDB
+  DEFAULT CHARSET=utf8
+  COLLATE=utf8_general_ci;
+
+
+CREATE TABLE test_db.oms_orders_2 (
+	id varchar(100) NOT NULL,
+	invoice_id varchar(100) NOT NULL,
+	order_address varchar(100) NULL,
+	created_at DATETIME NOT NULL
+)
+  ENGINE=InnoDB
+  DEFAULT CHARSET=utf8
+  COLLATE=utf8_general_ci;
+
+
+CREATE TABLE test_db.oms_orders_3 (
+	id varchar(100) NOT NULL,
+	invoice_id varchar(100) NOT NULL,
+	order_address varchar(100) NULL,
+	created_at DATETIME NOT NULL
+)
+  ENGINE=InnoDB
+  DEFAULT CHARSET=utf8
+  COLLATE=utf8_general_ci;
+
+CREATE INDEX oms_orders_1_id_IDX USING BTREE ON test_db.oms_orders_1 (id);
+CREATE INDEX oms_orders_1_id_IDX USING BTREE ON test_db.oms_orders_2 (id);
+CREATE INDEX oms_orders_1_id_IDX USING BTREE ON test_db.oms_orders_3 (id);
